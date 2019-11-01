b6cd78ddff6c8b42e1bea9c3e2723fb35c8fddae
WindBlade <vn.chemgio@yahoo.com>
2019-11-01 11:17:15 +0700

    simple bot

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const fs = require("fs");
 const login = require("facebook-chat-api");
 const readline = require("readline");
+var request = require('request');
 
 const option = {
     logLevel:"silent",
@@ -14,6 +15,11 @@ const option = {
 
 function reLogin (){
     let  dataLogin = {email: process.env.FB_USERNAME, password: process.env.FB_PASSWORD};
+    let rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
 
     login(dataLogin, option, (err, api) => {
         if(err) {
@@ -31,116 +37,82 @@ function reLogin (){
             return;
         }
         // Logged in wirite cookie!
-        fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState())).then(function () {
-            startBot ();
+        fs.writeFile('appstate.json', JSON.stringify(api.getAppState()),function (err) {
+            if(err)
+                return  console.log(err);
+
+            return startBot ();
         });
     });
 }
 
 function startBot (){
-    login({
-        appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))
-    }, function(err, api) {
-        if (err) {
-            console.error(err);
-            reLogin();
-        }
+    console.log("==============restart BOT");
+    fs.readFile('appstate.json',{encoding:'utf8'}, function (err, data) {
+        if(err)
+            return reLogin();
 
-        api.setOptions({
-            selfListen: false,
-            logLevel: option.logLevel,
-            updatePresence: false,
-            userAgent: option.userAgent
-        });
-
-
-        let yourId = api.getCurrentUserID(); //lấy Id người login hiện tại
-
-
-        api.listen(function callback(err, message) {
-            //block icon: fix bug khi nhận đc icon
-            if (message.body == '') {
-                api.sendMessage("Bot không hiểu bạn nói. Xin lỗi nha :(", message.threadID);
-                return;
+        login({
+            appState: JSON.parse(data)
+        }, function(err, api) {
+            if (err) {
+                console.error(err);
+                return reLogin();
             }
+            console.log("============== BOT READY");
+            api.setOptions({
+                selfListen: false,
+                logLevel: option.logLevel,
+                updatePresence: false,
+                userAgent: option.userAgent
+            });
 
-            //block all group : Chỗ này block all nhóm chát, k thíc thì comment lại
-            if (message.isGroup) return console.log("block all group");
-            //Simsimi
 
+            let yourId = api.getCurrentUserID(); //lấy Id người login hiện tại
 
-            if (message.body == "bot" || message.body == "Bot") {
-                botStatusThreads[message.threadID] = true;
-                isSimsimi = true;
-                api.sendMessage("Đã bật chế độ nói chuyện với bot (gõ offbot để tắt). Bắt đầu nào!", message.threadID);
-                return console.log("On sim");
-            } else if (message.body == "offbot" || message.body == "Offbot") {
-                isSimsimi = false;
-                botStatusThreads[message.threadID] = false;
-                api.sendMessage("Đã tắt chế độ nói chuyện với bot.", message.threadID);
-            }
-
-            if (isSimsimi && botStatusThreads.hasOwnProperty(message.threadID)) {
-                var user = yourId + "_" + message.threadID;
-                console.log(user);
-                //#1. use simsimi
-                useSimsimi(message.threadID,message.body,api);
-
-                return console.log("Pet next");
-            }
-
-            if (!answeredThreads.hasOwnProperty(message.threadID)) {
-
-                //Chức năng này dành cho người muốn bỏ qua ID nào đó
-                // Tìm id ở đây https://findmyfbid.in/
-                // Thêm 1 người vào chỉ cần thêm dấu ,"ID người"
-                // Group cũng thế
-
-                //if(blockGroupChat(message.threadID)){
-                //	return;
-                //};
-                if (blockUserChat(message.threadID)) {
+            api.listen(function callback(err, message) {
+                //block icon: fix bug khi nhận đc icon
+                if (message.body == '') {
+                    api.sendMessage("[From: Chủ tịch]Nói gì vậy, nói lại nghe coi :(", message.threadID);
                     return;
-                };
+                }
 
-                answeredThreads[message.threadID] = true;
-                api.sendMessage("Tin nhắn trả lời tự động.\n- Trả lời `bot` để nói chuyện đỡ buồn.", message.threadID);
-            }
+                //block all group : Chỗ này block all nhóm chát, k thíc thì comment lại
+                if (message.isGroup) return console.log("block all group");
+                //Simsimi
+
+                if (botStatusThreads.hasOwnProperty(message.threadID)) {
+                    api.sendMessage(" [From: Chủ tịch]: Đã ghi nhận yêu cầu của bạn, chủ tịch sẽ phản hồi sớm nhất có thể", message.threadID);
+                }
+
+                if (!answeredThreads.hasOwnProperty(message.threadID)) {
+
+                    //Chức năng này dành cho người muốn bỏ qua ID nào đó
+                    // Tìm id ở đây https://findmyfbid.in/
+                    // Thêm 1 người vào chỉ cần thêm dấu ,"ID người"
+                    // Group cũng thế
+
+                    //if(blockGroupChat(message.threadID)){
+                    //	return;
+                    //};
+                    if (blockUserChat(message.threadID)) {
+                        return;
+                    };
+
+                    answeredThreads[message.threadID] = true;
+                    api.sendMessage("[From: Chủ tịch]: Chủ tịch đang bận, sẽ phản hồi bạn ngay khi có thể", message.threadID);
+                }
+            });
+
         });
+    })
+    
 
-    });
 }
 
-var request = require('request');
 
 var answeredThreads = {};
 var botStatusThreads = {};
-var isSimsimi = false;
-
-const simsimi = require('simsimi')({
-    key: process.env.SIMSIMI_KEY, //key get here: https://workshop.simsimi.com
-    lang: "vn",
-    atext_bad_prob_max: 0.0, // Chỉ số nói tục
-    atext_bad_prob_min: 0.0,
-});
-useSimsimi = function (threadID, text, api) {
-    (async () => {
-        try {
-            if (blockGroupChat(threadID)) {
-                return;
-            };
-            if (blockUserChat(threadID)) {
-                return;
-            };
-            const response = await simsimi(text);
-
-            api.sendMessage(response, threadID);
-        } catch (e){
-            console.log("==============",e);
-            api.sendMessage("Pet không hiểu bạn nói. Xin lỗi nha :(", threadID);
-        }
-    })();
-};
 
 blockGroupChat = function (threadID) {
     var blockGroupIds = ["id gourup chat", "id gourup chat"];
